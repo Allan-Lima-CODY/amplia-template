a964ae216b251a0680905fe389020ca09c7a180d
Allan Lima <80070674+Allan-Lima-CODY@users.noreply.github.com>
2024-05-21 11:03:00 -0300

    Update CustomerGeneralInfo.vue

--- src/views/Customers/CustomerGeneralInfo.vue
+++ src/views/Customers/CustomerGeneralInfo.vue
@@ -54,7 +54,7 @@ export default defineComponent({
     },
     created() {
         if (this.formData.address === undefined) {
-            this.formData.status = false;
+            this.formData.status = true;
             this.formData.address = AddressService.defaultFields();
         }
 
