3ae8979393e456d4f04515280f867aa252b4c1fe
Arthur Brites <arth.brites@gmail.com>
2024-05-30 11:09:30 -0300

    fix: login modal double toggle bug and added error on trying to edit a feature with empty name

--- src/views/Login/Index.vue
+++ src/views/Login/Index.vue
@@ -1,10 +1,6 @@
 <script lang="ts">
 import { ref, toRefs, reactive } from 'vue'
 
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faEye, faEyeSlash, faUser } from '@fortawesome/free-solid-svg-icons'
-
 import { GenericFunctions } from '@/services/GenericFunctions'
 import { ModalService } from '@/services/ModalService'
 import { UserService } from '@/services/UsersService'
@@ -19,20 +15,20 @@ import LabelFields from '@/components/Forms/Labels/LabelFields.vue'
 import InputForms from '@/components/Forms/InputFields/InputForms.vue'
 import DarkModeSwitcher from '@/components/Header/DarkModeSwitcher.vue'
 import type { Login } from '@/models/User'
+import VisibilityButton from '@/components/Buttons/VisibilityButton.vue'
 
-library.add(faEye, faEyeSlash, faUser)
 
 
 export default {
     components: {
-        FontAwesomeIcon,
         PresentationLayout,
         ButtonApresentation,
         LabelInformation,
         ModalBase,
         LabelFields,
         InputForms,
-        DarkModeSwitcher
+        DarkModeSwitcher,
+        VisibilityButton
     },
     data() {
         const loginFields: Login = reactive(UserService.LoginDefaultFields());
@@ -125,42 +121,24 @@ input::-ms-clear {
     <DarkModeSwitcher class="hidden" />
     <PresentationLayout card-title="LOGIN" :handle="login">
         <template v-slot:slot1>
-            <div>
-                <div class="relative">
-                    <LabelFields label="E-mail" for-html="email"></LabelFields>
-                    <InputForms id="email" type="text" placeholder="Digite seu email" v-model="loginData.email"
-                       />
-                    <LabelInformation v-if="!emailValid" label="Email inválido!" color="text-red" />
-                </div>
+            <div class="relative">
+                <LabelFields label="E-mail" for-html="email"></LabelFields>
+                <InputForms id="email" type="text" placeholder="Digite seu email" v-model="loginData.email"
+                    />
+                <LabelInformation v-if="!emailValid" label="Email inválido!" color="text-red" />
             </div>
 
-            <div>
-                <div class="relative">
-                    <LabelFields label="Senha" for-html="password"></LabelFields>
-                    <InputForms id="password" :type="inputType" placeholder="Digite sua senha"
-                        v-model="loginData.password">
-                        <button type="button" @click.prevent="togglePasswordVisibility" class="absolute right-3 mt-4 cursor-pointer">
-                            <svg v-if="eyeIconVisible" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                            </svg>
+            <div class="relative">
+                <LabelFields label="Senha" for-html="password"></LabelFields>
+                <InputForms id="password" :type="inputType" placeholder="Digite sua senha"
+                    v-model="loginData.password">
+                    <VisibilityButton :eyeIconVisible="eyeIconVisible" @toggle-visibility="togglePasswordVisibility" />
 
-                            <svg v-if="!eyeIconVisible" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
-                            </svg>
-                        </button>
-
-                        <div class="text-left bottom-4 text-sm font-medium text-gray-900 dark:text-white mt-6 relative">
-                            <a href="#" @click.prevent="forgotPassword" class="hover:text-blue-500">Esqueceu a
-                                senha?</a>
-                        </div>
-                    </InputForms>
-                </div>
+                    <div class="text-left bottom-4 text-sm font-medium text-gray-900 dark:text-white mt-6 relative">
+                        <a href="#" @click.prevent="forgotPassword" class="hover:text-blue-500">Esqueceu a
+                            senha?</a>
+                    </div>
+                </InputForms>
             </div>
 
             <ButtonApresentation label="Login" />
--- src/views/PasswordReset/Index.vue
+++ src/views/PasswordReset/Index.vue
@@ -1,10 +1,6 @@
 <script lang="ts">
 import { ref, reactive, toRefs } from 'vue'
 
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faEye, faEyeSlash, faUser } from '@fortawesome/free-solid-svg-icons'
-
 import type { ModalInfo } from '@/models/ModalInfo'
 
 import { ModalService } from '@/services/ModalService'
@@ -14,20 +10,20 @@ import ButtonApresentation from '@/components/Buttons/ButtonPresentation.vue';
 import ApresentationLayout from '@/layouts/PresentationLayout.vue';
 import LabelFields from '@/components/Forms/Labels/LabelFields.vue'
 import InputForms from '@/components/Forms/InputFields/InputForms.vue'
+import VisibilityButton from '@/components/Buttons/VisibilityButton.vue'
 
 import DarkModeSwitcher from '@/components/Header/DarkModeSwitcher.vue'
 
-library.add(faEye, faEyeSlash, faUser)
 
 export default {
     components: {
-        FontAwesomeIcon,
         ModalBase,
         ButtonApresentation,
         ApresentationLayout,
         LabelFields,
         InputForms,
-        DarkModeSwitcher
+        DarkModeSwitcher,
+        VisibilityButton
     },
     data() {
         const modalInfo: ModalInfo = reactive(ModalService.getLoginModal());
@@ -100,21 +96,7 @@ input::-ms-clear {
                 <div class="relative">
                     <LabelFields label="Senha" for-html="password"></LabelFields>
                     <InputForms id="password" :type="inputPassword" placeholder="Digite sua senha" v-model="password">
-                        <button id="passwordButton" @click.prevent="togglePasswordVisibility" class="absolute right-3 mt-4 cursor-pointer">
-                            <svg v-if="eyeIconVisiblePassword" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                            </svg>
-
-                            <svg v-if="!eyeIconVisiblePassword" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
-                            </svg>
-                        </button>
+                        <VisibilityButton :eyeIconVisible="eyeIconVisiblePassword" @toggle-visibility="togglePasswordVisibility" />
                     </InputForms>
                 </div>
             </div>
@@ -124,21 +106,7 @@ input::-ms-clear {
                     <LabelFields label="Confirmar Senha" for-html="password"></LabelFields>
                     <InputForms id="confirmPassword" :type="inputConfirmPassword" placeholder="Confirme sua senha"
                         v-model="confirmPassword">
-                        <button id="confirmPasswordButton" @click.prevent="toggleConfirmPasswordVisibility" class="absolute right-3 mt-4 cursor-pointer">
-                            <svg v-if="eyeIconVisibleConfirmPassword" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                            </svg>
-
-                            <svg v-if="!eyeIconVisibleConfirmPassword" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
-                            </svg>
-                        </button>
+                        <VisibilityButton :eyeIconVisible="eyeIconVisibleConfirmPassword" @toggle-visibility="toggleConfirmPasswordVisibility" />
                     </InputForms>
                 </div>
             </div>
